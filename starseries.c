1f8c723302b262819a7e8be37d580c74a82c4958
yashagrawal812 <75661317+yashagrawal812@users.noreply.github.com>
2020-12-21 15:46:11 +0530

    Add files via upload

---
+++ starseries.c
@@ -0,0 +1,16 @@
+//program of printing * ladder by Yash Agrawal MCA 1 B 
+#include<stdio.h>
+#include<conio.h>
+ int main()
+   {
+   	 int initial,initial_1;
+   	 for(initial=1;initial<=5;initial++)
+   	  {
+   	  	for(initial_1=1;initial_1<=initial;initial_1++)
+   	  	 {
+   	  	 	printf("*");
+	     }
+	    printf("\n");
+	  }
+	 return 0;
+}
